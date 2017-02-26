ac4e87d778fb73c2c13ddcccd2155e87e7e738db
horstwurm <horst.wurm@bluewin.ch>
2017-02-26 18:04:38 +0000

    dig_signage

--- app/models/signage.rb
+++ app/models/signage.rb
@@ -1,7 +1,7 @@
 class Signage < ApplicationRecord
     belongs_to :signage_camp
     
-    has_attached_file :avatar, default_url: "/images/:style/missing.png", :styles => {:native => "1200x1000", :big => "1200x1000#", :medium => "250x250#"}
+    has_attached_file :avatar, default_url: "/images/:style/missing.png", :styles => {:native => "1200x1000", :big => "1200x1000#", :medium => "250x250#",:small => "50x50#"}
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
