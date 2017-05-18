3db1bd1860a6ba0905e2e91c773037df7db7934b
horstwurm <horst.wurm@bluewin.ch>
2017-05-18 16:14:26 +0000

    dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -320,8 +320,8 @@ def dashboard_projectdata
         @projects = Mobject.where("mtype=?", "Projekte")
         msg = []
         @projects.each do |p|
-          @kosten = Timetrack.select("sum(amount) as summe").where('mobject_id=? and costortime=?',p.id, "Kosten").group(summe).first
-          @aufwand = Timetrack.select("sum(amount) as summe").where('mobject_id=? and costortime=?',p.id, "Aufwand").group(summe).first
+          @kosten = Timetrack.select("sum(amount) as summe").where('mobject_id=? and costortime=?',p.id, "Kosten").first
+          @aufwand = Timetrack.select("sum(amount) as summe").where('mobject_id=? and costortime=?',p.id, "Aufwand").first
           msg << {:id => p.id, :kategorie => "Kosten", :summe => @kosten.summe}
           msg << {:id => p.id, :kategorie => "Aufwand", :summe => @aufwand.summe}
         end
