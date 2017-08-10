bf42349cce4bac48ec44f38eef36f2da7dfcb5f5
horstwurm <horst.wurm@bluewin.ch>
2017-08-10 21:18:46 +0000

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,9 +74,9 @@ Rails.application.configure do
       :port           => '25',
       :domain         => 'genotec.ch',
       :authentication => :plain,
-      :enable_starttls_auto => true
-      :user_name      => ENV.fetch('SMTP_USERNAME'),
-      :password       => ENV.fetch('SMTP_PASSWORD'),
+      :enable_starttls_auto => true,
+      :user_name      => ENV('SMTP_USERNAME'),
+      :password       => ENV('SMTP_PASSWORD'),
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
