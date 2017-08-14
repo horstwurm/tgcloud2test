e694c0cb31fc676c4bc32d36f7ff9c0a30ca5eef
horstwurm <horst.wurm@bluewin.ch>
2017-08-14 21:12:45 +0000

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,19 +66,13 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "tkbnet_#{Rails.env}"
   config.action_mailer.perform_caching = false
   # CR001
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :mailgun
   config.action_mailer.default_url_options = { host: 'mytgcloud.herokuapp.com' }
   
-  ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.mailgun.org',
-      :port           => '587',
-      :domain         => 'mg.mytgcloud.com',
-      :authentication => :plain,
-      :enable_starttls_auto => true,
-      :user_name      => ENV['SMTP_USERNAME'],
-      :password       => ENV['SMTP_PASSWORD'],
+  config.action_mailer.mailgun_settings = {
+    api_key: 'key-8543570e5be692fe5bf4f9983ce6dfd6',
+    domain: 'mg.mytgcloud.com',
   }
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
---
+++ config/initializers/mailgun.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# Add new mime types for use in respond_to blocks:
+# Mime::Type.register "text/richtext", :rtf
+
+Mailgun.configure do |config|
+  config.api_key = 'key-8543570e5be692fe5bf4f9983ce6dfd6'
+end
